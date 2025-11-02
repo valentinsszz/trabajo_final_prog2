e32bd766cd75be79486cad86c273a3492aa641ff
andresagustingarciaa <andres.agusting95@gmail.com>
2025-11-02 02:16:18 -0300

    agregue cliente.py

--- Base/cliente.py
+++ Base/cliente.py
@@ -1,3 +1,39 @@
-class Cliente:
+class Cliente:                 #lo mismo, nose si hacia falta poner los tipos de datoss
+    
+    # Constructor
+    
+    def __init__(self, numeroId, nombres, apellidos, email):
+        self.__numeroId = numeroId
+        self.__nombres = nombres
+        self.__apellidos = apellidos
+        self.__email = email
 
-    pass
+    
+    # Comandos (setters)
+    
+    def establecerNumeroId(self, numeroId):
+        self.__numeroId = numeroId
+
+    def establecerNombres(self, nombres):
+        self.__nombres = nombres
+
+    def establecerApellidos(self, apellidos):
+        self.__apellidos = apellidos
+
+    def establecerEmail(self, email):
+        self.__email = email
+
+    # Consultas
+    
+    def obtenerNumeroId(self):
+        return self.__numeroId
+
+    def obtenerNombres(self):
+        return self.__nombres
+
+    def obtenerApellidos(self):
+        return self.__apellidos
+
+    def obtenerEmail(self):
+        return self.__email
+
