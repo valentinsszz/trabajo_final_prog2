b93929647cdb89d63d1a32e3e8a8c99e76d6b196
valentinsszz <valentinsuarez2345@gmail.com>
2025-11-02 15:28:56 -0300

    Add Sucursal

--- Base/sucursal.py
+++ Base/sucursal.py
@@ -1,3 +1,31 @@
 class Sucursal:
+    def __init__(self, numeroId, direccion):
+        self.numeroId = numeroId
+        self.direccion = direccion
+        self.venta = []
 
-    pass
+    #comandos
+    def establecerNumeroId(self, numeroId):
+        self.numeroId = numeroId
+    
+    def establecerDireccion(self, direccion):
+        self.direccion = direccion
+
+    def agregarVenta(self, venta):
+        self.venta.append(venta)
+
+    def removerVenta(self, venta):
+        self.venta.remove(venta)
+    
+    #Consultas
+    def obtenerNumeroId(self):
+        return self.numeroId
+    
+    def obtenerDireccion(self):
+        return self.direccion
+    
+    def obtenerVentas(self):
+        return self.venta
+        
+
+    
