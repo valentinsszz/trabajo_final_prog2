9c2e52e14022f58b64c69346fecbf3c68cfb55df
andresagustingarciaa <andres.agusting95@gmail.com>
2025-11-02 02:41:12 -0300

    Agregue clase auto.py

--- Base/auto.py
+++ Base/auto.py
@@ -3,4 +3,33 @@ import vehiculo
 
 class Auto(vehiculo.Vehiculo):
 
-    pass
+    def __init__(self, numero_id, marca, modelo, anio,
+                 sucursal_id, estado_id,
+                 airbags, frenos_abs, caballos_fuerza):
+        # Llamamos al constructor de la clase padre Vehiculo
+        super().__init__(numero_id, marca, modelo, anio, sucursal_id, estado_id)
+        
+        # Atributos propios de Auto
+        self.__airbags = airbags
+        self.__frenos_abs = frenos_abs
+        self.__caballos_fuerza = caballos_fuerza
+
+    # --- Comandos ---
+    def establecerAirbags(self, airbags):
+        self.__airbags = airbags
+
+    def establecerFrenosAbs(self, frenos_abs):
+        self.__frenos_abs = frenos_abs
+
+    def establecerCaballosFuerza(self, caballos_fuerza):
+        self.__caballos_fuerza = caballos_fuerza
+
+    # --- Consultas ---
+    def obtenerAirbags(self):
+        return self.__airbags
+
+    def obtenerFrenosAbs(self):
+        return self.__frenos_abs
+
+    def obtenerCaballosFuerza(self):
+        return self.__caballos_fuerza
